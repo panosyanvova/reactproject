4efd452e186040ef4a09de2c51c718857d6d84f5
Vova Panosyan <panosyanvova.ait@gmail.com>
2022-07-17 14:08:32 +0400

    reacti ejy sarcel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    React.createElement("div", {className:"App"},
+      React.createElement("header",{className:"App-header"},
+        React.createElement("img", {src : logo , className:"App-logo" , alt:"logo"},),
+        
+        React.createElement("p", null,"Edit <code>src/App.js</code> and save to reload."),
+        
+        React.createElement("a" , {className:"App-link" , href:"https://reactjs.org" , target:"blank" , rel:"noopener noreferrer"}, "Learn React")
+      )
+    )
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
